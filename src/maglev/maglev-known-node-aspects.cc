8537ddc6584396fb555afd372d9364dc57fd19ed
Victor Gomes <victorgomes@chromium.org>
2026-07-16 04:58:33 -0700

    Revert "[maglev] Skip stale maps when narrowing after failed map compares"
    This reverts commit a08e6898870f7f0d0a07bd53078c1a5a11105b53.
    
    Reason for revert: Reverting main CLs, too many failures
    
    Original change's description:
    > [maglev] Skip stale maps when narrowing after failed map compares
    >
    > Fixed: 535384620
    > Change-Id: Ic806a6536e4618d9006bf82468304538fc3f3824
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8107195
    > Reviewed-by: Marja Hölttä <marja@chromium.org>
    > Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    > Commit-Queue: Marja Hölttä <marja@chromium.org>
    > Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#108684}
    
    Bug: 535384620
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5c5ffaf7d33d6add4629da81d0c3188405626b4a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8107778
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#108686}

--- src/maglev/maglev-known-node-aspects.cc
+++ src/maglev/maglev-known-node-aspects.cc
@@ -793,10 +793,7 @@ void KnownNodeAspects::NarrowPossibleMaps(Zone* zone,
                                           ValueNode* object,
                                           const PossibleMaps& excluded_maps) {
   NodeInfo* info = TryGetInfoFor(object);
-  if (info == nullptr || !info->possible_maps_are_known() ||
-      info->maps_are_stale()) {
-    return;
-  }
+  if (info == nullptr || !info->possible_maps_are_known()) return;
   PossibleMaps remaining = info->possible_maps();
   for (compiler::MapRef map : excluded_maps) {
     remaining.remove(map, zone);
