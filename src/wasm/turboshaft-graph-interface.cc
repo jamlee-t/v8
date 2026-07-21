bb02b4347a785daf9a2f93ca32a0ef7a5c63323a
Manos Koukoutos <manoskouk@chromium.org>
2026-07-21 09:52:21 +0000

    [wasm][shared] Remove CHECKs for shared data segments
    As long as we do not have shared functions/instances, there is no
    motivation to support shared element and data segments.
    
    Bug: 42204563, 536952804
    Change-Id: I2d31938ddf78ef2517822c877b19feb79850edbe
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8130722
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#108785}

--- src/wasm/turboshaft-graph-interface.cc
+++ src/wasm/turboshaft-graph-interface.cc
@@ -5094,12 +5094,6 @@ class TurboshaftGraphBuildingInterface
   }
 
   void DataDrop(FullDecoder* decoder, const IndexImmediate& imm) {
-    // TODO(14616): Fix sharedness.
-    // TODO(14616): Data segments aren't available during streaming compilation,
-    // hence the `has_shared()` check below. Discussion:
-    // github.com/WebAssembly/shared-everything-threads/issues/83
-    CHECK(!decoder->enabled_.has_shared() ||
-          !decoder->module_->data_segments[imm.index].shared);
     auto sig = FixedSizeSignature<MachineType>::Params(MachineType::Pointer(),
                                                        MachineType::Uint32());
     CallC(
